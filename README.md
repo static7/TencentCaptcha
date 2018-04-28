66692479cba435909f2ffb19224aef5f0d047574
static7 <wusong@jinvovo.com>
2018-04-28 16:35:58 +0800

    项目初始化和提交

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
 # TencentCaptcha
 腾讯验证码
+## 适合范围
+thinkphp5.1.x 专用
+
+## 使用说明
+
+* php版本 >=7.0.0
+
+### key申请
+传送门 [https://open.captcha.qq.com/](https://open.captcha.qq.com/)
+
+> 在config/你的模块名/config.php 配置
+
+```php
+    //腾讯验证码
+    'tencent_captcha'=>[
+        'aid'=>'xxxx',
+        'AppSecretKey'=>'xxxx**'
+    ],
+```
+
+> 前端页面配置  
+> 我是用的 layui 写的一个测试例子  
+> 具体参考页面 [https://007.qq.com/captcha/#/gettingStart](https://007.qq.com/captcha/#/gettingStart)
+```html
+<form class="layui-form" action="{:Url::build('Example/submit')}" method="post">
+    //表单代码省略 ...
+    
+    <div class="layui-form-item">
+        <div class="layui-input-block">
+            <input type="hidden" name="ticket" value="">
+            <input type="hidden" name="randstr" value="">
+            <button class="layui-btn" lay-submit="" lay-filter="demo1">立即提交</button>
+        </div>
+    </div>
+</form>
+```
+```javascript 1.6
+<script src="https://ssl.captcha.qq.com/TCaptcha.js"></script>
+<script type="text/javascript">
+    layui.use(['jquery', 'form'], function () {
+        var captcha = new TencentCaptcha('2068882069', function(result) {
+            console.log(result);
+            if (result.ret===2){
+                layui.layer.msg('你干嘛关闭呢?');
+                return false;
+            }
+            layui.$("input[name='ticket']").val(result.ticket);
+            layui.$("input[name='randstr']").val(result.randstr);
+            var form = layui.$("form");
+            layui.$.post(form.attr("action"), form.serialize(), function (data){
+                if (data.code===0){
+                    layui.layer.msg(data.msg);
+                    return false;
+                }
+                layui.layer.msg(data.msg);
+            });
+        });
+        layui.form.on('submit(demo1)', function () {
+            captcha.show();
+            return false;
+        });
+    });
+</script>
+```
+
+> 控制器 Example里的 submit 方法
+
+```php
+    /**
+     * Submit
+     * @author staitc7 <static7@qq.com>
+     * @return mixed
+     * @throws \think\Exception
+     */
+    public function submit()
+    {
+        $param=$this->app->request->param();
+        $Captcha=new Captcha();
+        // 两种方式 一种
+//        $result=$Captcha->setRandstr($param['randstr'])->setTicket($param['ticket'])->verify();
+        //或者
+        $result=$Captcha->verify($param['ticket'],$param['randstr']);
+        if ((int)$result['response']===0){
+            return $this->result($param,0,$result['err_msg']);
+        }
+        return $this->result($param,1,'成功');
+    }
+```
+
+
+
+
+
+
---
+++ src/Captcha.php
@@ -0,0 +1,185 @@
+<?php
+/**
+ * Description of Captcha.php.
+ * User: static7 <static7@qq.com>
+ * Date: 2018/4/28 9:25
+ */
+
+namespace static7\captcha;
+
+use think\Exception;
+use think\facade\{
+    Config, Request
+};
+
+class Captcha
+{
+    //票据
+    private $ticket;
+    //随机字符串
+    private $randstr;
+    //IP
+    private $ip;
+    //aid
+    private $aid;
+    //AppSecretKey
+    private   $AppSecretKey;
+    //url
+    protected $url = 'https://ssl.captcha.qq.com/ticket/verify';
+
+    /**
+     * Captcha constructor.
+     * * @param array $config
+     * * @throws Exception
+     */
+    public function __construct(array $config = [])
+    {
+        if (empty($config)) {
+            $config = Config::get('config.tencent_captcha');
+        } else if (empty($config['aid']) || empty($config['AppSecretKey'])) {
+            throw new Exception('配置 aid 和 AppSecretKey 不能为空');
+        }
+        $this->aid          = $config['aid'];
+        $this->AppSecretKey = $config['AppSecretKey'];
+    }
+
+    /**
+     * 获取配置
+     * @author staitc7 <static7@qq.com>
+     * @return mixed
+     */
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    /**
+     * 设置票据
+     * @author staitc7 <static7@qq.com>
+     * @param string $ticket 票据
+     * @return mixed
+     */
+    public function setTicket($ticket = '')
+    {
+        empty($this->ticket) && $this->ticket = $ticket;
+        return $this;
+    }
+
+    /**
+     * 获取票据
+     * @author staitc7 <static7@qq.com>
+     * @return mixed
+     */
+    public function getTicket()
+    {
+        return $this->ticket;
+    }
+
+    /**
+     * 设置ip
+     * @author staitc7 <static7@qq.com>
+     * @param string $ip IP
+     * @return mixed
+     */
+    public function setIp($ip = '')
+    {
+        empty($this->ip) && $this->ip = $ip ?: Request::ip(0, true);
+        return $this;
+    }
+
+    /**
+     * 获取ip
+     * @author staitc7 <static7@qq.com>
+     * @return mixed
+     */
+    public function getIp()
+    {
+        return $this->ip ?: Request::ip(0, true);
+    }
+
+    /**
+     * 设置随机字符串
+     * @author staitc7 <static7@qq.com>
+     * @param string $string
+     * @return mixed
+     */
+    public function setRandstr($string = '')
+    {
+        empty($this->randstr) && $this->randstr = $string;
+        return $this;
+    }
+
+    /**
+     * 获取随机字符串
+     * @author staitc7 <static7@qq.com>
+     * @return mixed
+     */
+    public function getRandstr()
+    {
+        return $this->randstr;
+    }
+
+
+    /**
+     * 验证
+     * @author staitc7 <static7@qq.com>
+     * @param string $ticket 票据
+     * @param string $randstr
+     * @param string $ip ip
+     * @return mixed
+     * @throws Exception
+     */
+    public function verify($ticket = null,$randstr=null, $ip = null)
+    {
+        $ticket && $this->setTicket($ticket);
+        $ip && $this->setip($ip);
+        $randstr && $this->setRandstr($randstr);
+        $param = [
+            'aid' => $this->aid,
+            'AppSecretKey' => $this->AppSecretKey,
+            'Ticket' => $this->getTicket(),
+            'Randstr' => $this->getRandstr(),
+            'UserIP' => $this->getIp()
+        ];
+        $data = $this->sendRequest($param);
+        return json_decode($data,true);
+    }
+
+    /**
+     * 请求
+     * @author staitc7 <static7@qq.com>
+     * @param string $url
+     * @param array  $param
+     * @param string $method
+     * @return mixed
+     * @throws Exception
+     */
+    public function sendRequest($param = [], $url = '', $method = 'get')
+    {
+        if ($url) {
+            $this->url = $url;
+        }
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_USERAGENT, Request::header('user-agent'));
+        $data = http_build_query($param ?? '');
+        if (strtolower($method) == 'get') {
+            curl_setopt($ch, CURLOPT_URL, $this->url . '?' . $data);
+        } else {
+            curl_setopt($ch, CURLOPT_URL, $this->url);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            $error = sprintf("curl[%s] error[%s]", $this->url, curl_errno($ch) . ':' . curl_error($ch));
+            throw new Exception($error);
+        }
+        curl_close($ch);
+        return $result;
+    }
+}
